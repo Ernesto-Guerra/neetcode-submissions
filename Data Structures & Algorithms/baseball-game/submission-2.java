4c7a269c6bbc7d3daf752ba16308dac9889c458d
Ernesto-Guerra <43183858+Ernesto-Guerra@users.noreply.github.com>
2026-06-16 19:14:58 -0600

    Add: baseball-game - submission-2

---
+++ Data Structures & Algorithms/baseball-game/submission-2.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int calPoints(String[] operations) {
+        Stack<Integer> scores = new Stack<>();
+
+        for(String s:operations){
+            if(s.equals("+")){
+                int top = scores.pop();
+                int newTop = top + scores.peek();
+                scores.push(top);
+                scores.push(newTop);
+            } else if(s.equals("D")){
+                scores.push(Integer.valueOf(scores.peek())*2);
+            } else if(s.equals("C")){
+                scores.pop();
+            } else{
+                scores.push(Integer.valueOf(s));
+            }
+        }
+
+        int sum = 0;
+
+        
+        return scores.stream().mapToInt(Integer::intValue).sum();
+    }
+}
