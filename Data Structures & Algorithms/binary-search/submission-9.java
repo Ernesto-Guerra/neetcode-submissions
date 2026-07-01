74ff9fabf4e544acb14ddf9b4e83554490fd08a0
Ernesto-Guerra <43183858+Ernesto-Guerra@users.noreply.github.com>
2026-07-01 17:00:06 -0600

    Add: binary-search - submission-9

---
+++ Data Structures & Algorithms/binary-search/submission-9.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        int l = 0;
+        int r = nums.length-1;
+
+        while(l<=r){
+            int i = l + (r-l)/2;
+            System.out.println(i);
+            int actual = nums[i];
+
+            if(actual == target){
+                return i;
+            }
+
+            if(actual<target){
+                l=i+1;
+            } else{
+                r=i-1;
+            }
+        }
+        return -1;
+    }
+}
